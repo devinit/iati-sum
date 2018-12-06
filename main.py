8294967027ba2a33c4cb497b37ca4e239d9b2b4d
Alex Miller <alex.k.miller@gmail.com>
2018-12-06 12:23:03 -0500

    Pub filter in python

--- main.py
+++ main.py
@@ -22,22 +22,109 @@ if __name__ == '__main__':
     header_frame = pd.DataFrame([full_header])
     header_frame.to_csv("/home/alex/git/iati-sum/sep/000header.csv", index=False, header=False, encoding="utf-8")
 
+    partner_pubs = [
+        "af",
+        "afdb",
+        "afd",
+        "aics",
+        "asdb",
+        "ausgov",
+        "enabel",
+        "be-dgd",
+        "bmgf",
+        "gac-amc",
+        "idrccrdi",
+        "finance_canada",
+        "caribank",
+        "cif",
+        "danida",
+        "ebrd",
+        "ec-devco",
+        "ec-echo",
+        "ec-near",
+        "ec-fpi",
+        "eib",
+        "finland_mfa",
+        "fao",
+        "france",
+        "bmz",
+        "theglobalfund",
+        "gpe",
+        "iadb",
+        "idlo",
+        "ifcwbg",
+        "ifad",
+        "idea",
+        "ilo",
+        "imf",
+        "iom",
+        "irishaid",
+        "jica",
+        "lithuania_mfa",
+        "millenniumchallenge",
+        "mofa-jpn",
+        "slovakaid",
+        "minbuza_nl",
+        "rvo",
+        "mfat",
+        "norad",
+        "ofid",
+        "odakorea",
+        "uasd",
+        "maec",
+        "sida",
+        "sdc_ch",
+        "beis",
+        "defra_transparency",
+        "dfid",
+        "dwp",
+        "deccadmin",
+        "dhsc",
+        "fco",
+        "hooda",
+        "mrc",
+        "ukmod",
+        "scottish_government",
+        "wfa",
+        "unw",
+        "unhabitat",
+        "utd",
+        "uncdf",
+        "cerf",
+        "unicef",
+        "undp",
+        "unesco",
+        "un-environment",
+        "unhcr",
+        "unido",
+        "unaids",
+        "unops",
+        "unocha",
+        "unfpa",
+        "wfp",
+        "unitedstates",
+        "usaid",
+        "worldbank",
+        "who",
+    ]
+
     # Loop through all the folders downloaded via IATI registry refresh, and pass XML roots to our flatten_activities function.
     for subdir, dirs, files in os.walk(rootdir):
         for filename in files:
             filepath = os.path.join(subdir, filename)
             publisher = os.path.basename(subdir)
-            print(filename)
-            try:
-                root = etree.parse(filepath).getroot()
-            except etree.XMLSyntaxError:
-                continue
-            output = iatiflat.flatten_activities(root)
-            if len(output) > 0:
-                data = pd.DataFrame(output)
-                data.columns = header
-                data['publisher_id'] = publisher
-                data.to_csv("/home/alex/git/iati-sum/sep/{}.csv".format(filename), index=False, header=False, encoding="utf-8")
+            if publisher in partner_pubs:
+                print(filename)
+                try:
+                    root = etree.parse(filepath).getroot()
+                except etree.XMLSyntaxError:
+                    continue
+                output = iatiflat.flatten_activities(root)
+                if len(output) > 0:
+                    data = pd.DataFrame(output)
+                    data.columns = header
+                    data['publisher_id'] = publisher
+                    data.to_csv("/home/alex/git/iati-sum/sep/{}.csv".format(filename), index=False, header=False, encoding="utf-8")
 
     # Once individual (headless) CSVs are written for each donor. It's an easy step to concatenate them into one large document.
     # You may want to consider doing this in code rather than saving each donor's CSVs to disk, but I found this useful for
